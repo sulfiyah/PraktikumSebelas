ca4c0c7be5a4e8afc2d15ec9e1cfca14005d6b2f
Falikha Sulfiyah <falikhasulfiyah@gmail.com>
2024-11-14 23:24:27 +0700

    Add files via upload

---
+++ matematika.py
@@ -0,0 +1,14 @@
+phi = 3.14
+
+def luas_lingkaran():
+  r = float(input("Masukkan jari-jari lingkaran: "))
+  return phi * r * r
+
+def luas_persegi():
+  s = float(input("Masukkan sisi persegi: "))
+  return s * s
+
+def luas_persegipanjang():
+  p = float(input("Masukkan panjang persegi panjang: "))
+  l = float(input("Masukkan lebar persegi panjang: "))
+  return p * l
